47f083d177dd83cb7ab9be294ca78d69a18a3733
LucSandes <45044738+LucSandes@users.noreply.github.com>
2020-03-30 20:19:11 -0300

    mudança adicionada pelo gitBash

--- Aula00/Aluno.java
+++ Aula00/Aluno.java
@@ -1,5 +1,22 @@
-public class Aluno{
-   private String nome;
-   private int id;
-   
+public class Aluno {
+	
+	private String nome;
+	private int id;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 }
