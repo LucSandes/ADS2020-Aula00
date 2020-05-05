3e14becb6660192e889f15aadb6889d0429687b9
LucSandes <45044738+LucSandes@users.noreply.github.com>
2020-05-05 18:46:12 -0300

    alteracao do id e name

--- Aula00/Aluno.java
+++ Aula00/Aluno.java
@@ -1,15 +1,7 @@
 public class Aluno {
 	
-	private String nome;
 	private int id;
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+	private String nome;
 
 	public int getId() {
 		return id;
@@ -17,6 +9,14 @@ public class Aluno {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 }
